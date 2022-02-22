44b64f49739d69a797d5f2855c9690aba9302e2d
mrzhuzhe <466648706@qq.com>
2022-02-22 11:29:35 +0800

    feature(): rl agent nt a=ent test test

--- conf/base.py
+++
@@ -1,2 +0,0 @@
-num_aggressive = 0 
-num_expand = 0
--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+from contextlib import redirect_stdout
+import io
+# Silence "Loading environment football failed: No module named 'gfootball'" message
+with redirect_stdout(io.StringIO()):
+    import kaggle_environments
 from typing import Dict
-import sys
-from agent import agent
+from lux_ai.rl_agent.rl_agent import agent
+# from lux_ai.handcrafted_agents.needs_name_v0 import agent
+
+
 if __name__ == "__main__":
-    
+
     def read_input():
         """
         Reads input from stdin
@@ -12,14 +19,16 @@ if __name__ == "__main__":
         except EOFError as eof:
             raise SystemExit(eof)
     step = 0
+
     class Observation(Dict[str, any]):
-        def __init__(self, player=0) -> None:
+        def __init__(self, player=0):
             self.player = player
             # self.updates = []
             # self.step = 0
     observation = Observation()
     observation["updates"] = []
     observation["step"] = 0
+    observation["remainingOverageTime"] = 60.
     player_id = 0
     while True:
         inputs = read_input()
@@ -28,6 +37,10 @@ if __name__ == "__main__":
         if step == 0:
             player_id = int(observation["updates"][0])
             observation.player = player_id
+            """
+            # fixes bug where updates array is shared, but the first update is agent dependent actually
+            observation["updates"][0] = f"{observation.player}"
+            """
         if inputs == "D_DONE":
             actions = agent(observation, None)
             observation["updates"] = []
