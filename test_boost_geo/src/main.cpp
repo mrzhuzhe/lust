f1283483f2c3afc05a05490a0acc4a1c8b3ae892
mrzhuzhe <466648706@qq.com>
2024-04-20 18:20:45 +0800

    2d 3d

--- test_boost_geo/src/main.cpp
+++ test_boost_geo/src/main.cpp
@@ -1,3 +1,5 @@
+// prepare for cgal  https://www.boost.org/doc/libs/1_85_0/libs/geometry/doc/html/geometry/quickstart.html
+
 #include <boost/geometry.hpp>
 #include <boost/geometry/geometries/point_xy.hpp>
 #include <boost/geometry/geometries/polygon.hpp>
@@ -6,7 +8,7 @@ using namespace boost::geometry;
 
 int main()
 {
-    model::d2::point_xy<int> p1(1, 1), p2(2, 2);
+    model::d3::point_xyz<int> p1(1, 1, 1), p2(2, 2, 2);
     std::cout << "Distance p1-p2 is: " << distance(p1, p2) << std::endl;
     return 0;
 }
