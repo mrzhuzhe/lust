bcf7c9c3cd1be187210cc48501ad058a4511a7f8
mrzhuzhe <466648706@qq.com>
2024-04-24 23:27:05 +0800

    polygon

--- test_boost_geo/src/spatial_index.cpp
+++ test_boost_geo/src/spatial_index.cpp
@@ -3,13 +3,17 @@
 #include <boost/geometry.hpp>
 #include <boost/geometry/geometries/point.hpp>
 #include <boost/geometry/geometries/box.hpp>
+#include <boost/geometry/geometries/geometries.hpp>
 
 #include <boost/geometry/index/rtree.hpp>
 
 #include <vector>
 #include <iostream>
+#include <fstream>
 
 #include <boost/foreach.hpp>
+
+#include <boost/geometry/io/svg/write_svg.hpp>
 
 namespace bg = boost::geometry;
 namespace bgi = boost::geometry::index;
@@ -18,8 +22,28 @@ typedef bg::model::point<float, 2, bg::cs::cartesian> point;
 typedef bg::model::box<point> box;
 typedef std::pair<box, unsigned> value;
 
+#include <boost/assign.hpp>
+using namespace boost::assign;
+
+
 // create the rtree using default constructor
 bgi::rtree< value, bgi::quadratic<16> > rtree;
+
+// template <typename Geometry1>
+// void create_svg(std::string const& filename, Geometry1 const& a)
+template <typename Geometry1, typename Geometry2>
+void create_svg(std::string const& filename, Geometry1 const& a, Geometry2 const& b)
+{
+    typedef typename boost::geometry::point_type<Geometry1>::type point_type;
+    std::ofstream svg(filename.c_str());
+
+    boost::geometry::svg_mapper<point_type> mapper(svg, 400, 400);
+    mapper.add(a);
+    mapper.add(b);
+
+    mapper.map(a, "fill-opacity:0.5;fill:rgb(153,204,0);stroke:rgb(153,204,0);stroke-width:2");
+    mapper.map(b, "opacity:0.8;fill:none;stroke:rgb(255,128,0);stroke-width:4;stroke-dasharray:1,7;stroke-linecap:round");
+}
 
 int main()
 {
@@ -56,6 +80,27 @@ int main()
     std::cout << "knn query result:" << std::endl;
     BOOST_FOREACH(value const& v, result_n)
     std::cout << bg::wkt<box>(v.first) << " - " << v.second << std::endl;
+
+    bg::model::ring<bg::model::d2::point_xy<float> > ring;
+
+
+    ring += 
+        bg::model::d2::point_xy<float>(4.0, -0.5), bg::model::d2::point_xy<float>(3.5, 1.0),
+        bg::model::d2::point_xy<float>(2.0, 1.5), bg::model::d2::point_xy<float>(3.5, 2.0),
+        bg::model::d2::point_xy<float>(4.0, 3.5), bg::model::d2::point_xy<float>(4.5, 2.0),
+        bg::model::d2::point_xy<float>(6.0, 1.5), bg::model::d2::point_xy<float>(4.5, 1.0),
+        bg::model::d2::point_xy<float>(4.0, -0.5);
+    // bg::append(ring, point(4.0, -0.5));
+    // bg::append(ring, point(3.5, 1.0));
+    // bg::append(ring, point(2.0, 1.5));
+    // bg::append(ring, point(3.5, 2.0));
+    // bg::append(ring, point(4.0, 3.5)); 
+    // bg::append(ring, point(4.5, 2.0));
+    // bg::append(ring, point(6.0, 1.5)); 
+    // bg::append(ring, point(4.5, 1.0));
+    // bg::append(ring, point(4.0, -0.5));
+
+    create_svg("./build/bin/test.svg", ring, result_s[1].first);
 
     return 0;
 }
