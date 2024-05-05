f634217ea37252ed6df905a42bf7d0f21783aca5
mrzhuzhe <466648706@qq.com>
2024-05-05 20:22:33 +0800

    collinear

--- test_cgal/src/simple.cpp
+++ test_cgal/src/simple.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <CGAL/Simple_cartesian.h>
-typedef CGAL::Simple_cartesian<double> Kernel;
+
+typedef CGAL::Simple_cartesian<float> Kernel;
 typedef Kernel::Point_2 Point_2;
 typedef Kernel::Segment_2 Segment_2;
+
 int main()
 {
   Point_2 p(1,1), q(10,10);
@@ -28,5 +30,34 @@ int main()
     break;
   }
   std::cout << " midpoint(p,q) = " << CGAL::midpoint(p,q) << std::endl;
+
+  {
+    Point_2 p(0, 0.3), q(1, 0.6), r(2, 0.9);
+    std::cout << (CGAL::collinear(p,q,r) ? "collinear\n" : "not collinear\n");
+  }
+  {
+    Point_2 p(0, 1.0/3.0), q(1, 2.0/3.0), r(2, 1);
+    std::cout << (CGAL::collinear(p,q,r) ? "collinear\n" : "not collinear\n");
+  }
+  {
+    Point_2 p(0,0), q(1, 1), r(2, 2);
+    std::cout << (CGAL::collinear(p,q,r) ? "collinear\n" : "not collinear\n");
+  }
+
+  Point_2 p2(0, 0.3), q2, r2(2, 0.9);
+  {
+    q2  = Point_2(1, 0.6);
+    std::cout << (CGAL::collinear(p2,q2,r2) ? "collinear\n" : "not collinear\n");
+  }
+  {
+    std::istringstream input("0 0.3   1 0.6   2 0.9");
+    input >> p2 >> q2 >> r2;
+    std::cout << (CGAL::collinear(p,q2,r2) ? "collinear\n" : "not collinear\n");
+  }
+  {
+    q2 = CGAL::midpoint(p2,r2);
+    std::cout << (CGAL::collinear(p2,q2,r2) ? "collinear\n" : "not collinear\n");
+  }
+
   return 0;
 }
