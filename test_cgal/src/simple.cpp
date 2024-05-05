b7915c75cabe4f90a0bafb9d3d24197cfc69b1ed
mrzhuzhe <466648706@qq.com>
2024-05-05 19:50:34 +0800

    cgal

--- test_cgal/src/simple.cpp
+++ test_cgal/src/simple.cpp
@@ -1,43 +1,32 @@
-// prepare for cgal  https://www.boost.org/doc/libs/1_85_0/libs/geometry/doc/html/geometry/quickstart.html
-
-#include <boost/geometry.hpp>
-#include <boost/geometry/geometries/point_xy.hpp>
-#include <boost/geometry/geometries/polygon.hpp>
-
-// #include <boost/geometry/geometries/adapted/c_array.hpp>
-
-// BOOST_GEOMETRY_REGISTER_C_ARRAY_CS(cs::cartesian)
-
-// #include <boost/geometry/geometries/adapted/boost_tuple.hpp>
-
-// BOOST_GEOMETRY_REGISTER_BOOST_TUPLE_CS(cs::cartesian)
-
-using namespace boost::geometry;
-typedef boost::geometry::model::point<float, 2, boost::geometry::cs::spherical_equatorial<boost::geometry::degree>> spherical_point;
-
+#include <iostream>
+#include <CGAL/Simple_cartesian.h>
+typedef CGAL::Simple_cartesian<double> Kernel;
+typedef Kernel::Point_2 Point_2;
+typedef Kernel::Segment_2 Segment_2;
 int main()
 {
-    model::d3::point_xyz<float> p1(1, 1, 1), p2(2, 2, 2);
-    std::cout << "Distance p1-p2 is: " << distance(p1, p2) << std::endl;
-
-    model::polygon<model::d2::point_xy<float> > poly;
-    //float points[][2] = {{1,2}, {3, 4}, {5, 6}, {7, 8}}; 
-    
-    model::d2::point_xy<float> points[] = {{-1 , 0}, {0, 1}, {1, 0}, {0.5, -0.5}, {-0.5, -0.5}}; 
-    append(poly, points);
-
-    model::d2::point_xy<float> p{0, 0};
-
-    std::cout << "Point p is in polygon? " << std::boolalpha << within(p, poly) << std::endl;
-
-    std::cout << "Area: " << area(poly) << std::endl;
-
-    // non-catercian 
-    spherical_point amsterdam(4.90, 52.37);
-    spherical_point paris(2.35, 48.86);
-
-    float const earth_radius = 3959; // miles
-    std::cout << "Distance in miles: " << distance(amsterdam, paris) * earth_radius << std::endl;
-
-    return 0;
+  Point_2 p(1,1), q(10,10);
+  std::cout << "p = " << p << std::endl;
+  std::cout << "q = " << q.x() << " " << q.y() << std::endl;
+  std::cout << "sqdist(p,q) = "
+            << CGAL::squared_distance(p,q) << std::endl;
+  Segment_2 s(p,q);
+  Point_2 m(5, 9);
+  std::cout << "m = " << m << std::endl;
+  std::cout << "sqdist(Segment_2(p,q), m) = "
+            << CGAL::squared_distance(s,m) << std::endl;
+  std::cout << "p, q, and m ";
+  switch (CGAL::orientation(p,q,m)){
+  case CGAL::COLLINEAR:
+    std::cout << "are collinear\n";
+    break;
+  case CGAL::LEFT_TURN:
+    std::cout << "make a left turn\n";
+    break;
+  case CGAL::RIGHT_TURN:
+    std::cout << "make a right turn\n";
+    break;
+  }
+  std::cout << " midpoint(p,q) = " << CGAL::midpoint(p,q) << std::endl;
+  return 0;
 }
