5a45fd5c55295eaa53a3435eb29e740e6edd1d1a
mrzhuzhe <466648706@qq.com>
2024-05-05 22:22:39 +0800

    reconstruction

--- test_cgal/src/simple.cpp
+++ test_cgal/src/simple.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
-#include <CGAL/Simple_cartesian.h>
+// #include <CGAL/Simple_cartesian.h>
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/convex_hull_2.h>
 
-typedef CGAL::Simple_cartesian<float> Kernel;
-typedef Kernel::Point_2 Point_2;
-typedef Kernel::Segment_2 Segment_2;
+// typedef CGAL::Simple_cartesian<float> Kernel;
+// typedef Kernel::Point_2 Point_2;
+// typedef Kernel::Segment_2 Segment_2;
+
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef K::Point_2 Point_2;
+typedef std::vector<Point_2> Points;
+typedef K::Segment_2 Segment_2;
+
+// typedef CGAL::Exact_predicates_inexact_constructions_kernel K3;
+// typedef CGAL::Projection_traits_yz_3<K3> K;
+// typedef K::Point_2 Point_2;
 
 int main()
 {
@@ -58,6 +69,15 @@ int main()
     q2 = CGAL::midpoint(p2,r2);
     std::cout << (CGAL::collinear(p2,q2,r2) ? "collinear\n" : "not collinear\n");
   }
+
+  Points points, result;
+  points.push_back(Point_2(0,0));
+  points.push_back(Point_2(10,0));
+  points.push_back(Point_2(10,10));
+  points.push_back(Point_2(6,5));
+  points.push_back(Point_2(4,1));
+  CGAL::convex_hull_2( points.begin(), points.end(), std::back_inserter(result) );
+  std::cout << result.size() << " points on the convex hull" << std::endl;
 
   return 0;
 }
---
+++ test_cgal/src/surface_mesh.cpp
@@ -0,0 +1,7 @@
+//  https://doc.cgal.org/latest/Manual/tuto_reconstruction.html
+#include <iostream>
+
+int main() {
+    std::cout << "123" << std::endl;
+    return 0;
+}
