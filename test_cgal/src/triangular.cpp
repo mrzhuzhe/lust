12b013cf9998bc18c4beebf834989d645b59ebad
mrzhuzhe <466648706@qq.com>
2024-05-18 00:02:21 +0800

    draw polygon

--- test_cgal/src/triangular.cpp
+++ test_cgal/src/triangular.cpp
@@ -1,16 +1,67 @@
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Delaunay_triangulation_2.h>
 #include <CGAL/Delaunay_triangulation_3.h>
 #include <CGAL/point_generators_3.h>
 #include <CGAL/draw_triangulation_3.h>
 typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
 typedef CGAL::Delaunay_triangulation_3<K>                   DT3;
 typedef CGAL::Creator_uniform_3<double,K::Point_3>          Creator;
+
+typedef CGAL::Delaunay_triangulation_2<K>                   DT2;
+
+#include <CGAL/Constrained_Delaunay_triangulation_2.h>
+#include <cassert>
+#include <iostream>
+typedef CGAL::Exact_predicates_tag                               Itag;
+typedef CGAL::Constrained_Delaunay_triangulation_2<K, CGAL::Default, Itag> CDT;
+typedef CDT::Point Point;
+typedef CDT::Edge  Edge;
+#include <CGAL/draw_triangulation_2.h>
+
+#include <CGAL/Polygon_2.h>
+
+#include <CGAL/draw_polygon_2.h>
+
+typedef CGAL::Polygon_2<K>                                  Polygon_2;
+typedef CGAL::Point_2<K>                                    P2;
+
 int main()
 {
-  std::vector<K::Point_3> points;
-  CGAL::Random_points_in_sphere_3<K::Point_3,Creator> g(1.0);
-  std::copy_n(g, 50, std::back_inserter(points));
-  DT3 dt3(points.begin(), points.end());
-  CGAL::draw(dt3);
+  // std::vector<K::Point_3> points;
+  // CGAL::Random_points_in_sphere_3<K::Point_3,Creator> g(1.0);
+  // std::copy_n(g, 50, std::back_inserter(points));
+  // DT3 dt3(points.begin(), points.end());
+  // CGAL::draw(dt3);
+
+  Polygon_2 points;
+  CDT cdt;
+  std::cout << "Inserting a grid of 5x5 constraints " << std::endl;
+  for (int i = 1; i < 6; ++i){
+    cdt.insert_constraint( Point(0,i), Point(6,i));
+    points.push_back(P2(0,i));
+    points.push_back(P2(6,i));
+  }    
+  for (int j = 1; j < 6; ++j){    
+    cdt.insert_constraint( Point(j,0), Point(j,6));
+    points.push_back(P2(j,0));
+    points.push_back(P2(j,6));
+  }
+  assert(cdt.is_valid());
+  int count = 0;
+  int count2 = 0;
+  for (const Edge& e : cdt.finite_edges()){
+    ++count2;
+    if (cdt.is_constrained(e))
+      ++count;
+  }
+    
+  std::cout << "The number of resulting constrained edges is  ";
+  std::cout <<  count << " " << count2 << std::endl;
+
+  //std::cout <<  cdt.size() << std::endl;
+  //DT2 dt2(points.begin(), points.end());
+  //CGAL::draw(cdt);
+  CGAL::draw(dt2);
+  CGAL::draw(points);
   return EXIT_SUCCESS;
 }
--- test_cgal/src/triangular2d.cpp
+++ test_cgal/src/triangular2d.cpp
@@ -18,6 +18,32 @@ typedef Finite_vertex_handles::iterator         Finite_vertex_handles_iterator;
 #include <CGAL/draw_triangulation_2.h>
 typedef Triangulation::Vertex_circulator Vertex_circulator;
 
+#include <CGAL/Delaunay_triangulation_2.h>
+typedef K::Iso_rectangle_2 Iso_rectangle_2;
+typedef K::Segment_2 Segment_2;
+typedef K::Ray_2 Ray_2;
+typedef K::Line_2 Line_2;
+typedef CGAL::Delaunay_triangulation_2<K>  Delaunay_triangulation_2;
+
+//A class to recover Voronoi diagram from stream.
+//Rays, lines and segments are cropped to a rectangle
+//so that only segments are stored
+struct Cropped_voronoi_from_delaunay{
+  std::list<Segment_2> m_cropped_vd;
+  Iso_rectangle_2 m_bbox;
+  Cropped_voronoi_from_delaunay(const Iso_rectangle_2& bbox):m_bbox(bbox){}
+  template <class RSL>
+  void crop_and_extract_segment(const RSL& rsl){
+    CGAL::Object obj = CGAL::intersection(rsl,m_bbox);
+    const Segment_2* s=CGAL::object_cast<Segment_2>(&obj);
+    if (s) m_cropped_vd.push_back(*s);
+  }
+  void operator<<(const Ray_2& ray)    { crop_and_extract_segment(ray); }
+  void operator<<(const Line_2& line)  { crop_and_extract_segment(line); }
+  void operator<<(const Segment_2& seg){ crop_and_extract_segment(seg); }
+};
+
+
 int main()
 {
   std::vector<Point> points =  { Point(0,0), Point(1,0), Point(0,1),
@@ -61,7 +87,22 @@ int main()
   // Triangulation t;
   // t.insert(begin, end);
 
-  CGAL::draw(T);
+  //CGAL::draw(T);
+
+
+  Delaunay_triangulation_2 dT;
+  dT.insert(points.begin(), points.end());
+
+  //construct a rectangle
+  Iso_rectangle_2 bbox(-1,-1,2,2);
+  Cropped_voronoi_from_delaunay vor(bbox);
+  
+  std::cout << "dt draw" << std::endl;
+  //extract the cropped Voronoi diagram
+  dT.draw_dual(vor);
+  //print the cropped Voronoi diagram as segments
+  std::copy(vor.m_cropped_vd.begin(),vor.m_cropped_vd.end(),
+    std::ostream_iterator<Segment_2>(std::cout,"\n"));
 
   return 0;
 }
