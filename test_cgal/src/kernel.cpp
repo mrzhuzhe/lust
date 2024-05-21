906bcefd78750183751d7db918890a662ff3cf42
mrzhuzhe <466648706@qq.com>
2024-05-21 23:28:26 +0800

    kernel

--- test_cgal/src/kernel.cpp
+++ test_cgal/src/kernel.cpp
@@ -1,4 +1,4 @@
-//  https://doc.cgal.org/latest/Kernel_23/index.html#Chapter_2D_and_3D_Geometry_Kernel
+//  kernel robustnesss https://doc.cgal.org/latest/Kernel_23/index.html#Chapter_2D_and_3D_Geometry_Kernel
 
 int main() {
     return 0;
