89685ba095d6fc9f96ad3aec8e7faa42af0a362f
mrzhuzhe <466648706@qq.com>
2024-05-27 22:10:34 +0800

    simple

---
+++ test_cgal_mesh/src/simple.cpp
@@ -0,0 +1,13 @@
+//	https://doc.cgal.org/latest/Polyhedron/index.html#Chapter_3D_Polyhedral_Surfaces
+#include <CGAL/Simple_cartesian.h>
+#include <CGAL/Polyhedron_3.h>
+typedef CGAL::Simple_cartesian<double>     Kernel;
+typedef CGAL::Polyhedron_3<Kernel>         Polyhedron;
+typedef Polyhedron::Halfedge_handle        Halfedge_handle;
+int main() {
+    Polyhedron P;
+    Halfedge_handle h = P.make_tetrahedron();
+    if ( P.is_tetrahedron(h))
+        return 0;
+    return 1;
+}
