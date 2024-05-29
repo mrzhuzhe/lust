5585fe2c37b5d63265f5df16915e70c5a1e6a3c0
mrzhuzhe <466648706@qq.com>
2024-05-29 22:30:56 +0800

    test kernel

--- vim_test/simple.cpp
+++ vim_test/simple.cpp
@@ -4,6 +4,7 @@ int main()
 {
 	std::cout << "test test" << std::endl;
 	std::cout << "append test" << std::endl;
-	
+	std::cout << "aaaappend    taaaappend aaaappend aaaaappend aaaappend daaaaaappend test" << std::endl;
+	std::cout << "dopy paste test" << std::endl;
 	return 0;
 }
