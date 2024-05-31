d9686346a55d7d0eadfe005491149273db2d632f
mrzhuzhe <466648706@qq.com>
2024-06-01 00:27:19 +0800

    trianglize

---
+++ test_cgal_mesh/src/3d_triangle.cpp
@@ -0,0 +1,12 @@
+/*
+https://projects.blender.org/blender/blender/issues/68277
+https://www.youtube.com/watch?v=sNxz_Ht6Y1Y
+https://matthias-research.github.io/pages/tenMinutePhysics/13-Tetrahedralize.pdf
+
+*/
+#include <iostream>
+
+int main() {
+	std::cout << "3d triangle test " << std::endl;
+	return 0;
+}
