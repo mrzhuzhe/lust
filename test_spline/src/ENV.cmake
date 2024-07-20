9d05a9d6779739974f6d1d49adad7bef9b9e6a05
mrzhuzhe <466648706@qq.com>
2024-07-20 22:42:45 +0800

    test spline

---
+++ test_spline/src/ENV.cmake
@@ -0,0 +1,9 @@
+set( BOOST_PATH ${PROJECT_SOURCE_DIR}/../../boost/boost_1_85_0/build)
+
+set(BOOST_INC ${BOOST_PATH}/include/)
+set(BOOST_LIBS ${BOOST_PATH}/lib)
+
+set(Eigen3_include_path ${PROJECT_SOURCE_DIR}/../../eigen/)
+
+set(opencv4_include_path /usr/local/include/opencv4)
+set(opencv4_libary_path /usr/local/lib)
