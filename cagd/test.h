8a000e6ad613a8f6f8ed06746cbac76d5a83311b
mrzhuzhe <466648706@qq.com>
2024-08-17 14:11:47 +0800

    short cut

--- cagd/test.h
+++ cagd/test.h
@@ -405,39 +405,37 @@ float curvature_0(bez_x,bez_y,weight,degree)
 }
 
 // ::::::::::::::
-// deboor.c
+// decas.c
 // ::::::::::::::
-float deboor(degree,coeff,knot,u,i)
+float decas(degree,coeff,t)
 /*
-	uses de Boor algorithm to compute one
-	coordinate on B-spline curve for param. value u in interval i.
-input:  degree:	polynomial degree of each piece of curve
-	coeff:	B-spline control points
-	knot:	knot sequence
-	u:	evaluation abscissa
-	i:	u's interval: u[i]<= u < u[i+1]
-output:	coordinate value. 
+     uses  de Casteljau to compute one coordinate
+     value of a  Bezier curve. Has to be called 
+     for each coordinate  (x,y, and/or z) of a control polygon.
+     Input:   degree: degree of curve.
+              coeff:  array with coefficients of curve.
+              t:      parameter value.
+      Output: coordinate value.
 */
 
-	float coeff[],knot[];
-	float u;
-	int degree,i;
+	float coeff[];
+	float t;
+	int degree;
 {
-	int k,j;
-	float t1,t2;
-	float coeffa[30];  /* might need adjustment! */
-	
-	for (j=i-degree+1; j<=i+1; j++)coeffa[j]=coeff[j];
+	int r,i;
+	float t1;
+	float coeffa[30];  /* an auxiliary array. Change dim. if too small*/
 
-	for (k=1; k<= degree; k++)
-	for ( j=i+1 ;j>=i-degree+k+1; j--)
-	{
-		t1= (knot[j+degree-k] - u )/(knot[j+degree-k]-knot[j-1]);
-		t2= 1.0-t1;
+	t1 = 1.0 - t;
+	for(i=0; i<=degree; i++) coeffa[i]=coeff[i]; /* save input */
 
-		coeffa[j]=t1* coeffa[j-1]+t2* coeffa[j];
-	}	
-	return (coeffa[i+1]);
+	for (r=1; r<= degree; r++)
+		for (i=0; i<= degree - r; i++)
+		{
+			coeffa[i]= t1* coeffa[i]  +   t * coeffa[i+1] ;
+		}
+
+	return (coeffa[0]);
 }
 
 
